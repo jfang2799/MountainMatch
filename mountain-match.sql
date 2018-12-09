603032973cb4d2414e81d255aa2147d056181e08
Rylan <rylanoc@gmail.com>
2018-12-08 22:31:46 -0500

    Updated db

--- mountain-match.sql
+++ mountain-match.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 05, 2018 at 09:29 PM
+-- Generation Time: Dec 09, 2018 at 04:29 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -25,59 +25,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `features`
---
-
-CREATE TABLE `features` (
-  `latitude` double DEFAULT NULL,
-  `longitude` double DEFAULT NULL,
-  `type` varchar(20) DEFAULT NULL,
-  `lip` tinyint(1) DEFAULT NULL,
-  `urban` tinyint(1) DEFAULT NULL,
-  `trail` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `features`
---
-
-INSERT INTO `features` (`latitude`, `longitude`, `type`, `lip`, `urban`, `trail`) VALUES
-(42.96581221408568, -72.90638688951731, 'Easy', 0, 0, 'easy_accurate'),
-(42.956470850771694, -72.90882367640735, 'Hard (Without Lip)', 0, 1, 'hard_accurate'),
-(42.9591482463886, -72.89613858796658, 'Hard (With Lip)', 1, 1, 'hard_accurate');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `jumps`
---
-
-CREATE TABLE `jumps` (
-  `latitude` double NOT NULL,
-  `longitude` double NOT NULL,
-  `type` varchar(20) DEFAULT NULL,
-  `size` int(11) NOT NULL,
-  `trail` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `jumps`
---
-
-INSERT INTO `jumps` (`latitude`, `longitude`, `type`, `size`, `trail`) VALUES
-(42.95686640058241, -72.8965998440981, 'Big Jump', 1000, 'hard_accurate'),
-(42.96206303133158, -72.90386326611042, 'Small Jump', 10, 'hard_innacurate');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `trails`
 --
 
 CREATE TABLE `trails` (
-  `latitude` double NOT NULL,
-  `longitude` double NOT NULL,
-  `name` varchar(20) DEFAULT NULL,
+  `name` varchar(20) NOT NULL,
   `difficulty` tinyint(4) NOT NULL,
   `accuracy` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -86,33 +38,25 @@ CREATE TABLE `trails` (
 -- Dumping data for table `trails`
 --
 
-INSERT INTO `trails` (`latitude`, `longitude`, `name`, `length`, `difficulty`, `accuracy`) VALUES
-(42.7284, 73.6918, 'easy_accurate', 802, 1, 5),
-(42.7295, 73.6928, 'easy_innacurate', 802, 1, 1),
-(42.7304, 73.6938, 'hard_accurate', 802, 5, 5),
-(42.7314, 73.6948, 'hard_innacurate', 802, 5, 1);
+INSERT INTO `trails` (`name`, `difficulty`, `accuracy`) VALUES
+('Boulder Pile', 4, 5),
+('Cooper\'s Junction', 2, 5),
+('Cut-Off', 3, 5),
+('Lodge', 3, 5),
+('Nitro', 3, 5),
+('Thanks Walt', 3, 5),
+('The Gulch', 3, 5),
+('Under Pressure', 3, 3);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `features`
---
-ALTER TABLE `features`
-  ADD UNIQUE KEY `latitude` (`latitude`);
-
---
--- Indexes for table `jumps`
---
-ALTER TABLE `jumps`
-  ADD UNIQUE KEY `latitude` (`latitude`);
-
---
 -- Indexes for table `trails`
 --
 ALTER TABLE `trails`
-  ADD UNIQUE KEY `latitude` (`latitude`);
+  ADD PRIMARY KEY (`name`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
